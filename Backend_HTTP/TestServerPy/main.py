ec19fc1ec5a91cc96092f996de93ea11b8b8f807
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-10-26 17:09:22 +0200

    Ok ora funzionano le chiamate fiche

--- Backend_HTTP/TestServerPy/main.py
+++ Backend_HTTP/TestServerPy/main.py
@@ -1,23 +1,20 @@
 from types import new_class
 from typing import Union
 from webbrowser import get
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, Json
 
 
-class Esp32:
-    temperature: float
-    light: float
-
-    def __init__(self, **data: BaseModel):
-        super().__init__(**data)
-        self.temperature = 0.0
-        self.light = 0.0
+class Esp32(BaseModel):
+    temperature: float = 0.0
+    light: float = 0.0
 
     # getter method
     def get_temp(self):
         return self.temperature
+
     def get_light(self):
         return self.light
 
@@ -26,16 +23,17 @@ class Esp32:
         self._age = x
 
         # setter method
+
     def set_temp(self, x):
         self.temperature = x
+
     def set_light(self, x):
         self.light = x
 
 
-
-
-
 esp = Esp32()
+
+# esp = Esp32()
 
 app = FastAPI()
 origins = ["*"]
@@ -49,26 +47,27 @@ app.add_middleware(
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.get("/api/data/{item_val}")
-def read_esp(item_val: int, q: Union[str, None] = None):
+async def read_esp(item_val: int, q: Union[str, None] = None):
     return {"item_id": item_val, "q": q}
 
-@app.post("/items/")
-async def create_item(item: BaseModel):
-    esp.set_light(2.0)
-    esp.set_temp(2.0)
-    print(item)
+
+@app.put("/items/")
+async def create_item(item: Esp32):
+    esp.set_light(item.get_temp())
+    esp.set_temp(item.get_light())
     return item
 
 
 @app.get("/esp/data/")
-def getStronzih():
+async def getStronzih():
     return {"temperatura": esp.get_temp(), "lux": esp.get_light()}
