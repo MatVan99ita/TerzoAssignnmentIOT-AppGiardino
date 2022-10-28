2ce71fd6594fbf3a8c790fa9159a8b3cdf698d44
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-10-28 16:43:38 +0200

    Roba su android ma ancora non va

--- Mobile/AndroidExample04/bt-client/src/main/java/unibo/btclient/MainActivity.java
+++ Mobile/AndroidExample04/bt-client/src/main/java/unibo/btclient/MainActivity.java
@@ -4,24 +4,18 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
 import android.util.Log;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import unibo.btlib.BluetoothChannel;
 import unibo.btlib.BluetoothUtils;
@@ -33,16 +27,23 @@ import unibo.btclient.utils.C;
 
 public class MainActivity extends AppCompatActivity {
     private BluetoothChannel btChannel;
-    private BluetoothAdapter adapter = null;
-    private Set<BluetoothDevice> Devices;
+    private BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
     private UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-
+    private BluetoothDevice hc05 = btAdapter.getRemoteDevice("00:14:03:05:F2:D9");
+    private boolean ledSwitch = false;
+    private BluetoothSocket socket = null;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.layout_brutto);
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            View child = layout.getChildAt(i);
+            child.setEnabled(false);
+        }
+        Button btButton = layout.findViewById(R.id.bt_connection_req);
+        btButton.setEnabled(true);
         final BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
         if (btAdapter != null && !btAdapter.isEnabled()) {
             startActivityForResult(
@@ -51,58 +52,45 @@ public class MainActivity extends AppCompatActivity {
             );
         }
 
-        initUI();
+        initUI(layout);
     }
 
-    private void initUI() {
-        adapter = BluetoothAdapter.getDefaultAdapter();
-        this.printami(adapter.getBondedDevices());
-        BluetoothDevice hc05 = adapter.getRemoteDevice("00:14:03:05:F2:D9");
-
-        BluetoothSocket socket = null;
-
+    private void initUI(ConstraintLayout layout) {
 
         findViewById(R.id.bt_connection_req).setOnClickListener(l -> {
             l.setEnabled(false);
             try {
                 pairDevices();
-            } catch (BluetoothDeviceNotFound bluetoothDeviceNotFound) {
-                Toast.makeText(this, "Bluetooth device not found !", Toast.LENGTH_LONG)
-                        .show();
-                bluetoothDeviceNotFound.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
                 l.setEnabled(true);
+                Button led1 = layout.findViewById(R.id.led1);
+                led1.setEnabled(true);
             }
         });
-
-        findViewById(R.id.sendBtn).setOnClickListener(l -> {
-            String message = ((EditText)findViewById(R.id.editText)).getText().toString();
-            btChannel.sendMessage(message);
-            ((EditText)findViewById(R.id.editText)).setText("");
+        findViewById(R.id.led1).setOnClickListener(l -> {
+            switchLed();
         });
+
     }
 
-    private void pairDevices() {
-        Devices = adapter.getBondedDevices();
-        List<String> list = new ArrayList();
-
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && Devices.size() > 0) {
-            Devices.forEach(el -> list.add(el.getName() + " - " + el.getAddress()));
+    private void switchLed() {
+        if(ledSwitch){
+            btChannel.sendMessage("0");
+        } else {
+            btChannel.sendMessage("1");
         }
-        else {
-            // In case no device is found
-            Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+        ledSwitch = !ledSwitch;
+    }
+
+    private void pairDevices() throws IOException {
+        if(socket == null || !socket.isConnected()){
+            this.socket = hc05.createInsecureRfcommSocketToServiceRecord(uuid);
+            System.out.println(socket.toString());
+            System.out.println(hc05);
+            System.out.println(btAdapter.toString());
         }
-
-        // Adding the devices to the list with ArrayAdapter class
-        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
-        devicelist.setAdapter(adapter);
-
-        // Method called when the device from the list is clicked
-        devicelist.setOnItemClickListener(myListListener);
     }
 
     @Override
@@ -170,8 +158,5 @@ public class MainActivity extends AppCompatActivity {
                 ));
             }
         }).execute();
-    }*/
-    private void printami(Object obj){
-        System.out.println(obj.toString());
-    }
+    }//*/
 }
