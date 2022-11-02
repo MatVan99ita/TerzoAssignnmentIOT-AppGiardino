a2cf5ffc9c253a7c21a16df9e3d211be9a77a944
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-11-02 15:36:03 +0100

    Eliminati file inutili

--- App_pc/HTML_Frontend/main.js
+++ App_pc/HTML_Frontend/main.js
@@ -2,15 +2,17 @@
 var xhttp = new XMLHttpRequest();
 var url = "http://localhost:8000/esp/data/"
 
-$(document).ready({
-  xhttp: onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      // Typical action to be performed when the document is ready:
-      document.getElementById("demo").innerHTML = xhttp.responseText;
-      console.log(xhttp.responseText);
-    }
-  },
-  xhttp:open("GET", url, true),
-  xhttp:send()
+$(document).ready(function(){ setInterval(getSensorData, 5000, url); });
 
-});
+
+function getSensorData(url){
+  $.getJSON(url, function(result){
+    $("#temperature_log").html(result["temperatura"] + "°C");
+    $("#light_log").html(result["lux"] + " Cd")
+    $.each(result, function(i, field){
+      $("#demo").append(field + " ");
+      console.log(field + " " + i);
+    });
+  });
+  return ;
+}
