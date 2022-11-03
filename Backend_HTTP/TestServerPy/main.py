b0de23c920734216822f9b74ece72711a981003d
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-11-03 17:11:57 +0100

    fatto cose con backend e arduino

--- Backend_HTTP/TestServerPy/main.py
+++ Backend_HTTP/TestServerPy/main.py
@@ -6,7 +6,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Json
 
-
 class Esp32(BaseModel):
     temperature: float = 0.0
     light: float = 0.0
