1bba5fd4e61331fe3414cae5d1e9160cf4bd8e38
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-11-04 09:34:47 +0100

    Server che parte come dovrebbe

---
+++ Backend_HTTP/TestServerPy/run.py
@@ -0,0 +1,38 @@
+import threading
+import multiprocessing
+import time
+import os
+from multiprocessing import Process
+
+
+# os.system("uvicorn --host 0.0.0.0 main:app --reload");
+def runHTTPServer():
+    os.system("uvicorn --host 0.0.0.0 main:app --reload")
+
+
+def runSerialCommunicator():
+    os.system("py ServerPy.py")
+
+
+def print_func(continent='Asia'):
+    print('The name of continent is : ', continent)
+
+
+if __name__ == "__main__":  # confirms that the code is under main function
+    procs = []
+    proc1 = Process(target=runHTTPServer)  # instantiating without any argument
+    proc2 = Process(target=runSerialCommunicator)
+
+    procs.append(proc1)
+    procs.append(proc2)
+
+
+    proc1.start()
+    proc2.start()
+
+
+"""
+    # complete the processes
+    for proc in procs:
+        proc.join()
+"""
