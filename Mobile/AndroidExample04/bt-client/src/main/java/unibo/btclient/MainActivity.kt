5a4c83d073893d5e1b206b5122da8b8dd41d139b
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-11-07 15:31:31 +0100

    Update MainActivity.kt

--- Mobile/AndroidExample04/bt-client/src/main/java/unibo/btclient/MainActivity.kt
+++ Mobile/AndroidExample04/bt-client/src/main/java/unibo/btclient/MainActivity.kt
@@ -3,7 +3,6 @@ package unibo.btclient
 import android.support.v7.app.AppCompatActivity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
-import android.widget.ArrayAdapter
 import android.os.Bundle
 import unibo.btclient.R
 import android.content.Intent
@@ -16,8 +15,7 @@ import android.os.AsyncTask
 import android.support.constraint.ConstraintLayout
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ListView
+import android.widget.*
 import unibo.btclient.utils.BluetoothChannel
 import unibo.btclient.utils.C
 import unibo.btlib.BluetoothUtils
@@ -36,11 +34,70 @@ class MainActivity : AppCompatActivity() {
         private var socket: BluetoothSocket = hc05.createRfcommSocketToServiceRecord(mUUID)
     }
 
-    //private lateinit var btChannel: BluetoothChannel
+    /**Gruppo di bottoni per la luce*/
+    private lateinit var btnLed1: Button
+    private lateinit var btnLed2: Button
+    private lateinit var btnLed3_up: Button
+    private lateinit var btnLed3_down: Button
+    private lateinit var btnLed4_up: Button
+    private lateinit var btnLed4_down: Button
+
+    /**Gruppo di bottoni per l'irrigazione*/
+    private lateinit var btnIrrigazione: Button
+    private lateinit var btnIrrigazione_fast: Button
+    private lateinit var btnIrrigazione_slow: Button
+
+    /**Gruppo di label*/
+    private lateinit var led3: TextView
+    private lateinit var led4: TextView
+    private lateinit var irrigazione: TextView
+
+
+    private lateinit var btnBell: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLed1 = findViewById(R.id.led1)
+        btnLed2 = findViewById(R.id.led2)
+
+        btnLed3_up = findViewById(R.id.led3plus)
+        btnLed3_down = findViewById(R.id.led3minus)
+
+        btnLed4_up = findViewById(R.id.led4plus)
+        btnLed4_down = findViewById(R.id.led4minus)
+
+        btnIrrigazione = findViewById(R.id.irrigation_onoff)
+        btnIrrigazione_fast = findViewById(R.id.irrigation_plus)
+        btnIrrigazione_slow = findViewById(R.id.irrigation_minus)
+        /*
+            * btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn.setEnabled(false);
+
+             new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    // This method will be executed once the timer is over
+                        btn.setEnabled(true);
+                        Log.d(TAG,"resend1");
+
+                }
+            },2000);// set time as per your requirement
+            }
+            });
+            *
+            * */
+
+        led3 = findViewById(R.id.led3_view)
+        led4 = findViewById(R.id.led4_view)
+        irrigazione = findViewById(R.id.irrigation_view)
+
+        btnBell = findViewById(R.id.alarm_btn)
+
         //btAdapter = BluetoothAdapter.getDefaultAdapter()
         //lv = findViewById<View>(R.id.listview) as ListView
         adapter = ArrayAdapter<Any?>(this, android.R.layout.simple_list_item_1)
@@ -59,14 +116,55 @@ class MainActivity : AppCompatActivity() {
                 pairDevices();
             } catch (e: IOException) {
                 e.printStackTrace();
-            } /*finally {
-                l.setEnabled(true);
-                Button led1 = layout.findViewById(R.id.led1);
-                led1.setEnabled(true);
-            }*/
+            }
         })
 
     }
+
+    /**
+     * Trovare un modo per ricordare se i led sono accesi e da lì inviare i comandi di accensione in base al led scelto
+     * o usando il nome o il numero della porta
+     */
+    private fun switchLed(led_num: Int){
+        val message: String = ""+led_num
+    }
+
+    /**
+     * invio della quantità di fade al led specificato
+     * @param fade_amount 0 .. 255
+     */
+    private fun fadeLed(led_num: Int, fade_amount: Int): Int {
+        val message: String = ""+led_num+"="
+        if(fade_amount < 0) {
+            message.plus(0)
+        }
+        else if(fade_amount > 255) {
+            message.plus(255)
+        }
+        else {
+            message.plus(fade_amount)
+        }
+        return fade_amount
+    }
+
+    /**
+     * invio dell'attivazione dell'irrigazione
+     *
+     * (nel controller del bottone va disattivato per n minuti se si ha ancora il controllo)
+     */
+    private fun irrigationStart(velocity: Int){
+        val message: String = ""+velocity
+    }
+
+    /**
+     * Funzione per l'allarme
+     */
+    private fun disableAlarm(){
+
+    }
+
+
+
 
     private fun pairDevices() {
         ConnectToDevice(this).execute()
@@ -75,18 +173,6 @@ class MainActivity : AppCompatActivity() {
         bt_led.setOnClickListener(View.OnClickListener {
             arduinoCommunication("BELIIIN")
         })
-    /*
-        if(!socket.isConnected()){
-
-            socket = hc05.createInsecureRfcommSocketToServiceRecord(this.mUUID)
-            //BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
-            socket!!.connect()
-            var bt_led: Button = findViewById(R.id.led1)
-            bt_led?.isEnabled = true
-            bt_led.setOnClickListener(View.OnClickListener {
-                arduinoCommunication()
-            })
-        }*/
     }
 
     private fun arduinoCommunication(input: String) {
