ba385e07d78b521feb89e2a0814c4ff67efac25f
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-11-07 15:49:40 +0100

    Esempio del servo a differenti velocità

--- Arduino/servo_velocity/servo_velocity.ino
+++ Arduino/servo_velocity/servo_velocity.ino
@@ -1,9 +1,33 @@
+
+#include <Servo.h>
+#define SERVO_PIN 9
+
+Servo myservo;
+int mapSpeed;
+int pos = 180;
 void setup() {
   // put your setup code here, to run once:
-
+  myservo.attach(9);
+  myservo.write(0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  moveto(60, 20);
+}
 
+int pos1 = 0;
+void moveto(int position, int speed){
+  mapSpeed = map(speed, 0, 30, 30, 0);
+
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(mapSpeed);                       // waits 15 ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(mapSpeed);                       // waits 15 ms for the servo to reach the position
+  }
+  
 }
