c362b98ed2cc75681306bb5aced218fc4d149c7f
LeroySmith17 <leroy.fabbri@studio.unibo.it>
2023-02-20 16:07:16 +0100

    Update
    Non lo so

--- Arduino/Pseudo_finale/Pseudo_finale.ino
+++ Arduino/Pseudo_finale/Pseudo_finale.ino
@@ -1,3 +1,16 @@
+#include <Servo.h>
+#define SERVO_PIN 9
+
+Servo myservo;
+int mapSpeed;
+int pos = 0;
+int mode = 0;   //0=auto, 1=manuale, 2=ALARM
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(9);
+  myservo.write(0);
+}
+
 CheckLight(){
   Light=1//leggi la luce
   if(Light<1000 && luce=0)
@@ -8,25 +21,47 @@ CheckLight(){
     {
     TaskSystem_off();
     }
+  else{
+    //scala la luce in base alla luminosità
+    luce3;
+    luce4;
+    }
 }
 
 CheckTemp(){
    Temperatura=1; //"readAnalogPin"
    if (Temperatura>Soglia){
+      "ALARM Mode activated";
+      mode=2;
+      //invia un messaggio, blocca tutto
       
+
+
+      //aspetta il messaggio dall'app
+    else if(Temperatura>soglia2){
+        //attiva irrigazione, scala la velocità in base alla temperatura
+        TaskIrrigatore;
+      }
     }
   
   }
 
 
 
-void setup() {
+
+void setup(){
   // put your setup code here, to run once:
+  myservo.attach(9);
+  myservo.write(0);
+  Mode=Auto;
+  }
 
-}
+void loop{
+  //attende un eventuale messaggio dall'app per modalità manuale
+  if(mode==0){
+  CheckLight();
+  CheckTemp();
+  delay(10000);
+  }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-   CheckLight();
-   CheckTemp();
 }
