a8a27460d43962489e02e62e70d6a9f8fef68cca
MatVan99ita <matteo.vanni2@studio.unibo.it>
2024-11-07 16:12:01 +0100

    finalemte capite un paio di cose in più

--- Arduino/SmartGarden/IrrigationTask.cpp
+++ Arduino/SmartGarden/IrrigationTask.cpp
@@ -12,6 +12,15 @@ void IrrigationTask::init(int period){
     Task::init(period);
 }
 
+/*
+Dall'esempio del blink-print task ogjni 500 millisecondi partono entrambe
+appena finisce  quella più lunga(cioè il print allungato) si riattiva la task led
+Dunque con il servo servirà una volta partita la task che si accenda, faccia le sue cose e si spenga
+però nel fare le sue cose lo deve fare per Y secondi -> questi Y secondi sono dettati dalla velocità 
+del servo che più fa caldo più dovrebbe andare in teoria mentre per il timing basta tenerlo fisso perchè 
+quello è la temporizzazzione che ogni secondo lo va a ricontrollare se è attivo e poi funziona per il periodo che gli è impostato in secondo tempo
+Il blocco di X minuti lo si mette a monte per evitare casini
+*/
 void IrrigationTask::tick(){
   this -> servo->on();
   this -> servo->setVelocity(servo_speed);
