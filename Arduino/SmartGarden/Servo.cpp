5cb7d6e0c4312ad65c0038533f08abc29c27f0ed
MatVan99ita <matteo.vanni2@studio.unibo.it>
2024-11-20 16:24:42 +0100

    sistematp

--- Arduino/SmartGarden/Servo.cpp
+++ Arduino/SmartGarden/Servo.cpp
@@ -23,31 +23,33 @@ void Servo::startRotation() {
     if(this->speed >= 0){//check for the delay equation
 
       int dilei = map(this->speed, 0, 4, MIN_VAL, MAX_VAL);
-
-      //Overkill for calculating bbut is self explanatory
+      int angle = 0;
+      //Angoli notevoli del servo
       int angle0 =   750 + 0   * 10;
-      int angle90 =  750 + 90  * 10;
+      //int angle90 =  750 + 90  * 10;
       int angle180 = 750 + 180 * 10;
 
+      
       motor.write(angle0);
-      Serial.print("ANGOLO :"); Serial.println(motor.read());
-      delay(dilei);
 
-      motor.write(angle90);
-      Serial.print("ANGOLO :"); Serial.println(motor.read());
-      delay(dilei);
+      for(int i = 0; i <= 180; i = i + this->speed * 10){
+        angle = 750 + i *10;
+        motor.write(angle);
+        Serial.print("ANGOLO SU :"); Serial.println(motor.read());
+        delay(dilei);
+      }
 
       motor.write(angle180);
-      Serial.print("ANGOLO :"); Serial.println(motor.read());
-      delay(dilei);
+      
+      for(int i = 180; i >= 0; i = i - this->speed * 10){
+        angle = 750 + i *10;
+        motor.write(angle);
+        Serial.print("ANGOLO GIU :"); Serial.println(motor.read());
+        delay(dilei);
+      }
+      
 
-      motor.write(angle90);
-      Serial.print("ANGOLO :"); Serial.println(motor.read());
-      delay(dilei);
-
-      motor.write(angle0);
-      Serial.print("ANGOLO :"); Serial.println(motor.read());
-      delay(dilei);
+      
 
       
      
